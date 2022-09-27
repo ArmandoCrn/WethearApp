483afe636cb0b8c75c61d9c279e5d1d0d421f9ca
Armando <hello.armandocrn@gmail.com>
2022-09-27 11:46:14 +0200

    style, done

--- src/weather.js
+++ src/weather.js
@@ -11,7 +11,6 @@ export default async function getMeteo() {
 
   const ourMeteo = {
     name: data.name,
-    desc: data.weather[0].main,
     alt: data.weather[0].description,
     icon: data.weather[0].icon,
     temp: Math.round(data.main.temp),
